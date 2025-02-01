51579c0c43ba82bd958a26c6d61f1d1ff504cfb6
Timur <40215181+ibragimovtimur1987@users.noreply.github.com>
2025-02-01 21:39:29 +0500

    refact

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using PrototypePattern;
+﻿using PrototypePattern;
 
 Console.WriteLine("=== Демонстрация клонирования объектов ===\n");
 
